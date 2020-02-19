fe3b750435cdbf8fd0546659c2e4684d025721e6
Tommy Ho <kuanweih@uw.edu>
2020-02-18 21:58:35 -0800

    demo ready w/o button

--- demo_camera.py
+++ demo_camera.py
@@ -13,6 +13,7 @@ import os
 import cv2
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+from gpiozero import Button
 
 mobilenet = keras.applications.mobilenet.MobileNet()
 x = mobilenet.layers[-6].output
@@ -21,6 +22,7 @@ predictions = Dense(7, activation='softmax')(x)
 model = Model(inputs=mobilenet.input, outputs=predictions)
 filepath = "model.h5"
 model.load_weights(filepath)
+button = Button(17)
 
 # Set up camera constants
 IM_WIDTH = 1280
@@ -89,7 +91,8 @@ for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=
             cv2.rectangle(frame, (IM_WIDTH // 2 - BOX_WIDTH, IM_HEIGHT // 2 - BOX_HEIGHT), (IM_WIDTH // 2 + BOX_WIDTH, IM_HEIGHT // 2 + BOX_HEIGHT), (0, 0, 255), 4)
             
     key_event = cv2.waitKey(1)
-    if key_event == ord('k'):
+    #if key_event == ord('k'):
+    if button.is_pressed:
         infer_timestamp = time.time()
         
         cropped_frame = input_frame[IM_HEIGHT // 2 - BOX_WIDTH : IM_HEIGHT // 2 + BOX_HEIGHT, IM_WIDTH // 2 - BOX_WIDTH : IM_WIDTH // 2 + BOX_HEIGHT]
